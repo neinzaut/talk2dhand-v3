3cf79ecf006ae471507af3a03514e6c6601cacfc
Francesca Tuazon <fmatuazon@gmail.com>
2025-11-24 00:25:46 +0800

    fix(config): remove outputFileTracingRoot to address warning

--- app/ui/next.config.ts
+++ app/ui/next.config.ts
@@ -6,9 +6,6 @@ const nextConfig: NextConfig = {
   
   // Enable standalone output for Docker
   output: 'standalone',
-  
-  // Set the workspace root to fix warning
-  outputFileTracingRoot: require('path').join(__dirname, '../../'),
 };
 
 export default nextConfig;
